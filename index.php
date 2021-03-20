2e4ba031e02eb35c82f10a3e70b2ec1ccc0944ca
mssahumanisha <67999137+mssahumanisha@users.noreply.github.com>
2021-03-20 16:49:31 +0530

    Task1

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link href="1.css" type="text/css" rel="stylesheet">
+		<title>Basic Banking System </title>
+	</head>
+    <body>
+		<div class="h">
+			<div class="ih nav nav-bar">
+				<nav>
+					<ul>
+						<li><a href="index.php" class="logo">Basic Banking System By Manisha Sahu</a></li>
+						<li><a href="his.php" class="link">Transaction History</a></li>
+						<li><a href="users.php" class="link">Users</a></li>
+						<li><a href="index.php" class="link">Home</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+		<section id="home">
+			<div class="container">
+			</div>
+		</section>
+
+		<section id="cus">
+		  <center style="color:purple"><h1><b><u>Axis Bank</u></b></h1>
+		  <h3>By Manisha Sahu</h3><br>
+		  </center>
+		 </section>
+		<div class="container">
+            <div class="row text-center" id="item_list">
+                <div class="col-sm-6">
+                    <a href="users.php" >
+                        <div class="circle">
+                            <img src="1.jpg" alt="">
+							<div class="caption">
+                                <h1>Users Details</h1>
+
+                            </div>
+                        </div>
+                    </a>
+                </div>
+				<div class="col-sm-6">
+                    <a href="his.php" >
+                        <div class="circle">
+                            <img src="2.jpg" alt="">
+                            <div class="caption">
+								<h1>Transaction History</h1>
+
+                            </div>
+                        </div>
+                    </a>
+                </div>
+			</div>
+	  </body>
+</html>
